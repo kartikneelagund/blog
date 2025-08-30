7bf4867fc0b106378925d2f4e33348096292193b
Kartik Neelagund <kartikneelagund8050@gmail.com>
2025-08-30 21:04:02 +0530

    added fronetend url new

--- backend/index.js
+++ backend/index.js
@@ -28,7 +28,7 @@ mongoose.connect(process.env.MONGO_URI)
 
 
 
-const allowedOrigins = ["http://localhost:5173","https://blog-33js.vercel.app/login"];
+const allowedOrigins = ["http://localhost:5173","https://blog-33js.vercel.app/"];
 
 app.use(cors({
   origin: allowedOrigins,
