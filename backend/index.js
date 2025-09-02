dfb7736d9f876cd75696b7c5879aed23a614bc88
Kartik Neelagund <kartikneelagund8050@gmail.com>
2025-09-02 12:09:26 +0530

    new admin files

--- backend/index.js
+++ backend/index.js
@@ -6,8 +6,11 @@ import cors from "cors";
 import authRoutes from "./routes/auth.js";
 import blogRoutes from "./routes/blog.js";
 import userRoutes from "./routes/user.js";
+import adminRoutes from "./routes/adminRoutes.js"; //  NEW
 
-dotenv.config(); // ✅ load .env first
+dotenv.config(); //load .env first
+
+
 
 const app = express();
 
@@ -36,21 +39,21 @@ app.use("/api/blogs", blogRoutes);
 app.use("/api/users", userRoutes);
 
 app.get("/", (req, res) => {
-  res.send("✅ Backend is running!");
+  res.send("Backend is running!");
 });
 
 // ==================
 // DB + Server Start
 // ==================
-console.log("MONGO_URI:", process.env.MONGO_URI ? "✅ Loaded" : "❌ Missing");
-console.log("JWT_SECRET:", process.env.JWT_SECRET ? "✅ Loaded" : "❌ Missing");
+console.log("MONGO_URI:", process.env.MONGO_URI ? "Loaded" : "❌ Missing");
+console.log("JWT_SECRET:", process.env.JWT_SECRET ? " Loaded" : "❌ Missing");
 
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     const PORT = process.env.PORT || 5000;
     app.listen(PORT, () =>
-      console.log(`🚀 Server running on port ${PORT}`)
+      console.log(` Server running on port ${PORT}`)
     );
   })
-  .catch((err) => console.error("❌ DB Connection Error:", err));
+  .catch((err) => console.error(" DB Connection Error:", err));
